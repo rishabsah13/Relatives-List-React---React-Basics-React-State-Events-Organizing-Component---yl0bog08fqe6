8e0a911b37aeceaac1619b9378ed5aca849724cd
rishabsah13 <106971462+rishabsah13@users.noreply.github.com>
2022-09-17 11:28:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+                <ol>
+                    <li key={relativeListItem1}>mr.x</li>
+                    <li key ={relativeListItem2>mr.y</li>
+                    <li key ={relativeListItem3>mr.z</li>
+                    <li key ={relativeListItem4>mr.k</li>
+                 </ol>
             </div>
         )
     }
